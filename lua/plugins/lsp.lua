0333dc1118bccc93910abf4f5950fe39e0e5a0f6
Patrick Podbregar <patrick.podbregar@gmail.com>
2024-09-21 21:30:35 +0200

    activate lspconfig and mason

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"neovim/nvim-lspconfig"
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			{ "williamboman/mason.nvim", opts = {} }
+		},
+		config = function()
+			require("mason-lspconfig").setup()
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end
+			})
+		end
+	}
+}
