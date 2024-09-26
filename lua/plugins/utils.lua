12383a8c2d5450fb9963930f02e5684c316e45aa
Patrick Podbregar <patrick.podbregar@gmail.com>
2024-09-26 10:13:15 +0200

    add neo-tree plugin and required utils

---
+++ lua/plugins/utils.lua
@@ -0,0 +1,20 @@
+return {
+	-- Neovim Lua funcion library
+	{
+		"nvim-lua/plenary.nvim",
+		lazy = true
+	},
+	{
+		"MunifTanjim/nui.nvim",
+		lazy = true
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+		lazy = true,
+		opts = {}
+	},
+	{
+		"3rd/image.nvim",
+		lazy = true
+	}
+}
