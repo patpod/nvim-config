25d868994dbf0831e1bf7bfe574155733f517b7c
Patrick Podbregar <patrick.podbregar@gmail.com>
2024-10-04 10:43:43 +0200

    Use <C-s> to save files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 -- [[ Basic Keymaps ]]
+-- Disable default behavior of space because it is used as our leader key
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- Save file
+vim.keymap.set({"i", "x", "n", "s"}, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
